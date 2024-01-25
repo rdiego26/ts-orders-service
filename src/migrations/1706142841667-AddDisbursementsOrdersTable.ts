e47653438d60b57c82fcd2b2b232e4fdaaa6a082
ramos <rdiego26@gmail.com>
2024-01-25 09:27:08 +0000

    fix: rename disbursements_id field

--- src/migrations/1706142841667-AddDisbursementsOrdersTable.ts
+++ src/migrations/1706142841667-AddDisbursementsOrdersTable.ts
@@ -7,12 +7,12 @@ export class AddDisbursementsOrdersTable1706142841667 implements MigrationInterf
               create table if not exists disbursements_orders
               (
                   id         uuid                                   not null primary key,
-                  disbursements_id uuid                           not null,
+                  disbursement_id uuid                           not null,
                   order_id text not null,
                   amount  numeric(5, 2) not null,
                   fee  numeric(5, 2) not null,
                   CONSTRAINT fk_disbursements
-                  FOREIGN KEY(disbursements_id)
+                  FOREIGN KEY(disbursement_id)
                   REFERENCES disbursements(id),
                   CONSTRAINT fk_orders
                   FOREIGN KEY(order_id)
